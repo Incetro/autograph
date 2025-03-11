3616fcfe52758c06fc021bd27b845936265a10fb
Gleb-Kovalenko <80492878+Gleb-Kovalenko@users.noreply.github.com>
2025-03-11 16:17:05 +0300

    fix: minor fixes

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Autograph",
+    platforms: [
+        .macOS(.v12)
+    ],
     products: [
         .library(
             name: "Autograph",
